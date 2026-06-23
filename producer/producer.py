f5cedee6ee3d4bc5d0d2e35df93a34ba4278442b
ABHIRAMA H H <abhiramahh2004@gmail.com>
2026-06-23 22:22:01 +0530

    Enhaced The Project

--- producer/producer.py
+++ producer/producer.py
@@ -11,14 +11,33 @@ producer = KafkaProducer(
 
 coins = ["BTC", "ETH", "SOL", "DOGE", "XRP"]
 
+exchanges = ["Binance", "Coinbase", "Kraken", "Bybit"]
+
+trade_types = ["BUY", "SELL"]
+
+regions = ["Asia", "Europe", "North America", "South America"]
+
 TOTAL_RECORDS = 1000
 
 for i in range(TOTAL_RECORDS):
 
     trade = {
+        "trade_id": f"TRD{i+1}",
+
         "coin": random.choice(coins),
+
+        "exchange": random.choice(exchanges),
+
         "price": round(random.uniform(100, 120000), 2),
+
         "volume": random.randint(1, 100),
+
+        "trade_type": random.choice(trade_types),
+
+        "trader_region": random.choice(regions),
+
+        "fee": round(random.uniform(1, 20), 2),
+
         "trade_time": datetime.now().isoformat()
     }
 
--- src/main/scala/Cryptostreaming.scala
+++ src/main/scala/Cryptostreaming.scala
@@ -21,9 +21,14 @@ object CryptoStreaming {
       .load()
 
     val schema = StructType(Array(
+      StructField("trade_id", StringType),
       StructField("coin", StringType),
+      StructField("exchange", StringType),
       StructField("price", DoubleType),
       StructField("volume", IntegerType),
+      StructField("trade_type", StringType),
+      StructField("trader_region", StringType),
+      StructField("fee", DoubleType),
       StructField("trade_time", StringType)
     ))
 
@@ -35,9 +40,39 @@ object CryptoStreaming {
         "trade_time",
         to_timestamp(col("trade_time"))
       )
+
       .withColumn(
         "trade_value",
         col("price") * col("volume")
+      )
+
+      .withColumn(
+        "net_trade_value",
+        col("price") * col("volume") - col("fee")
+      )
+
+      .withColumn(
+        "price_category",
+        when(col("price") < 20000, "Low")
+          .when(col("price") < 70000, "Medium")
+          .otherwise("High")
+      )
+
+      .withColumn(
+        "volume_category",
+        when(col("volume") < 30, "Small")
+          .when(col("volume") < 70, "Medium")
+          .otherwise("Large")
+      )
+
+      .withColumn(
+        "hour",
+        hour(col("trade_time"))
+      )
+
+      .withColumn(
+        "trade_day",
+        date_format(col("trade_time"), "yyyy-MM-dd")
       )
 
     val query = parsedDF.writeStream
